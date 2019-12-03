1d2ee8db8be8eecb72fec0f75172307509953f8d
Matt Zimmerman <matt.zimmerman@ccri.com>
2019-12-03 16:59:04 -0500

    Little further along on day 3

--- 3/day3.py
+++ 3/day3.py
@@ -8,13 +8,17 @@ class Point(PointTuple):
 
 class Wire():
     PATSTEP = re.compile(r'([UDLR])(\d+)')
+    DIR = {'U': Point(0,1), 'D': Point(0,-1), 'L': Point(-1,0), 'R': Point(1,0)}
     
-    def __init__(self, text):
+    def __init__(self, text, debug=False):
         self.text = text
+        self.debug = debug
         self.elements = self._parse_text(self.text)
         #print("Created object 3 %s" % (text,))
         
     def _parse_text(self, text):
+        if self.debug:
+            print(text)
         elements = set()
         curr = Point(0, 0)
         elements.add(curr)
@@ -22,16 +26,19 @@ class Wire():
             m = Wire.PATSTEP.match(step)
             if m:
                 d = m.group(1)
-                n = m.group(2)
-                print(d, n)
-            curr = self._add_step(curr, step)
-        return set()
+                n = int(m.group(2))
+                for i in range(n):
+                    curr = curr.add(Wire.DIR[d])
+                    elements.add(curr)
+                    if self.debug:
+                        print(curr)
+        return elements
             
     def _add_step(self, start, step):
         print(start, step)
         return start
         
 if __name__ == '__main__':
-    w1 = Wire("R8,U5,L5,D3")
-    print(w1)
+    w1 = Wire("R8,U5,L5,D3", debug=True)
+    w2 = Wire("U7,R6,D4,L4", debug=True)
     
