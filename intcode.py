b7256abdf5b33aeddc2b6e023c655fda10f94f05
Matt Zimmerman <matt.zimmerman@ccri.com>
2019-12-05 17:37:11 -0500

    Started day 5

---
+++ intcode.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+class Op:
+    def __init__(self, name, opcode, params):
+        self.name = name
+        self.opcode = opcode
+        self.params = params
+        
+    def __repr__(self):
+        return ("Op(name=%s opcode=%d params=%d)" % (self.name, self.opcode, self.params))
+
+class Intcode:
+    OPS = [
+        Op("Add", 1, 3),
+        Op("Mult", 2, 3),
+        Op("Input", 3, 1),
+        Op("Output", 4, 1),
+        Op("Stop", 99, 0)]
+    OPS_LOOKUP = {}
+    for op in OPS:
+        OPS_LOOKUP[op.opcode] = op
+    
+    def __init__(self, csvdata, debug=False):
+        self.initdata = np.array(csvdata.split(','), dtype=np.int64)
+        self.debug = debug
+        
+
+    def run(self, noun=None, verb=None):
+        p = 0
+        data = np.copy(self.initdata)
+        if noun != None:
+            data[1] = noun
+        if verb != None:
+            data[2] = verb
+        if self.debug: self.print_data(data)
+        while True:
+            opcode = data[p]
+            if opcode == 99:
+                return data[0]
+            a = data[p+1]
+            b = data[p+2]
+            c = data[p+3]
+            if opcode == 1:
+                data[c] = data[a] + data[b]
+            elif opcode == 2:
+                data[c] = data[a] * data[b]
+            else:
+                raise Exception("bad opcode %d at p=%d" % (opcode, p))
+            if self.debug: self.print_data(data)
+            p = p + 4
+
+    def run_all(self, stop=None):
+        for n in range(100):
+            for v in range(100):
+                out = self.run(n, v)
+                if self.debug: print("noun=%2d verb=%2d out=%d" % (n, v, out))
+                if out == stop:
+                    return (n, v)
+
+    def print_data(self, data):
+        print(",".join(data.astype(str)))
+
+if __name__ == '__main__':
+    test = Intcode('1,9,10,3,2,3,11,0,99,30,40,50', debug=True)
+    test.run()
+    print(test.OPS)
+    print(test.OPS_LOOKUP)
