ead0ab963ed8d8975c2427b4e69fda2048e90848
Matthew Zimmerman <matt.zimmerman@ccri.com>
2019-12-16 09:00:34 -0500

    First half of day 12

--- day12.py
+++ day12.py
@@ -20,17 +20,44 @@ class Planet:
     def __repr__(self):
         return "Planet(pos=%s vel=%s)" % (self.pos, self.vel)
 
+    def pe(self):
+        return abs(self.pos.x) + abs(self.pos.y) + abs(self.pos.z)
+    
+    def ke(self):
+        return abs(self.vel.x) + abs(self.vel.y) + abs(self.vel.z)
+    
+    def te(self):
+        return self.pe() * self.ke()
 
 class Day12:
     def __init__(self, planets):
+        self.t = 0
         self.planets = planets
 
     def step(self):
-        pass
+        for i, a in enumerate(self.planets):
+            for b in self.planets[i+1:]:
+                self.apply_gravity(a, b)
+        for p in self.planets:
+            p.pos = p.pos.add(p.vel)
+        self.t += 1
 
     def apply_gravity(self, a, b):
-        pass
+        for dim in ['x', 'y', 'z']:
+            if getattr(a.pos, dim) > getattr(b.pos, dim):
+                setattr(a.vel, dim, getattr(a.vel, dim)-1)
+                setattr(b.vel, dim, getattr(b.vel, dim)+1)
+            elif getattr(a.pos, dim) < getattr(b.pos, dim):
+                setattr(a.vel, dim, getattr(a.vel, dim)+1)
+                setattr(b.vel, dim, getattr(b.vel, dim)-1)
+            else:
+                pass
         
+    def system_energy(self):
+        te = 0
+        for p in self.planets:
+            te += p.te()
+        return te
         
 if __name__ == '__main__':
     test1 = Day12([
@@ -39,4 +66,33 @@ if __name__ == '__main__':
                 Planet(4, -8, 8),
                 Planet(3, 5, -1)
             ])
+    for _ in range(10):
+        test1.step()
+    for p in test1.planets:
+        print(p)
+    print(test1.system_energy())
+    
+    test2 = Day12([
+                Planet(-8, -10, 0),
+                Planet(5, 5, 10),
+                Planet(2, -7, 3),
+                Planet(9, -8, -3)
+            ])
+    for _ in range(100):
+        test2.step()
+    for p in test2.planets:
+        print(p)
+    print(test2.system_energy())
+    
+    inp = Day12([
+                Planet(-13, 14, -7),
+                Planet(-18, 9, 0),
+                Planet(0, -3, -3),
+                Planet(-15, 3, -13)
+            ])
+    for _ in range(1000):
+        inp.step()
+    for p in inp.planets:
+        print(p)
+    print(inp.system_energy())
     
