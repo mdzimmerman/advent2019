47de2fc403ee41e5c1037f79e49f7ca6b55a2a11
Matt Zimmerman <matt.zimmerman@ccri.com>
2019-12-30 13:24:44 -0500

    Second half of day 16

--- 16/day16.py
+++ 16/day16.py
@@ -24,29 +24,50 @@ def build_row(l, n, patt):
     #print(lneed, n, repeat)
     return (repeat * m)[1:l+1]
 
-def apply_phases(inp, n):
+def fft(inp, nphases):
     x = np.array([int(d) for d in inp], dtype=np.int64)
     mat = build_matrix(x.shape[0])
-    for _ in range(n):
+    for _ in range(nphases):
         x = abs(mat.dot(x)) % 10
     return "".join([str(d) for d in x])
 
+def fastfft(inp, nphases):
+    offset = int(inp[:7])
+    #print("offset = %d" % (offset,))
+    inpfull = (inp * 10_000)[offset:]
+    #print("len(inpfull) = %d" % (len(inpfull),))
+    xlen = len(inpfull)
+    x = np.array([int(d) for d in inpfull], dtype=np.int64)
+    for n in range(nphases):
+        #print("# n=%d" % (n,))
+        xnew = np.zeros(shape=(xlen,), dtype=np.int64)
+        xsum = 0
+        for i in range(xlen-1, -1, -1):
+            xsum += x[i]
+            xnew[i] = xsum
+        x = abs(xnew) % 10
+    return "".join([str(d) for d in x[:8]])
+
 if __name__ == '__main__':
-    print(apply_phases("12345678", 4))
-    print(apply_phases("80871224585914546619083218645595", 100)[:8])
-    
     inp = ""
     with open("input.txt", "r") as fh:
         for l in fh:
             inp += l.rstrip()
-    print(len(inp))
-    print(apply_phases(inp, 100)[:8])
-    print(len(inp * 10_000))
-    print(apply_phases(inp * 10_000, 100)[:100])
-    #X = build_matrix(8)
-    #a = np.array([1, 2, 3, 4, 5, 6, 7, 8], dtype=np.int64)
-    #b = X.dot(a)
-    #print(abs(b) % 10)
-    #for i in range(1, 9):
-    #    print(build_row(8, i, [0, 1, 0, -1]))
-    #print(build_row(8, 2, [0, 1, 0, -1]))
+    print("len(inp) = %d" % (len(inp),))
+
+    print()
+    print("-- part1 --")
+    print("tests")
+    print(fft("12345678", 4))
+    print(fft("80871224585914546619083218645595", 100)[:8])
+    print("input")
+    print(fft(inp, 100)[:8])
+    
+    print()
+    print("-- part2 --")
+    print("tests")
+    print(fastfft("03036732577212944063491565474664", 100))
+    print(fastfft("02935109699940807407585447034323", 100))
+    print(fastfft("03081770884921959731165446850517", 100))
+    print("input")
+    print(fastfft(inp, 100))
