f70be2ec013e716a5af7c6d6a4e3ebb49655e794
Matthew Zimmerman <matt.zimmerman@ccri.com>
2020-01-03 21:47:24 -0500

    second half of day 12

--- 12/day12.py
+++ 12/day12.py
@@ -6,12 +6,27 @@ Created on Sun Dec 15 22:07:51 2019
 """
 
 from copy import deepcopy
+from functools import reduce
 import sys
 
 if '..' not in sys.path:
     sys.path.append('..')
 
 from point import Vec3
+
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:      
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
+
+def lcmm(*args):
+    """Return lcm of args."""   
+    return reduce(lcm, args)
 
 class Planet:
     def __init__(self, x, y, z):
@@ -68,6 +83,48 @@ class Day12:
             print("  %s" % (p,))
         print(")")
 
+class System:
+    def __init__(self, planets):
+        self.initpos=[[], [], []]
+        self.initvel=[[], [], []]
+        self.nplanets = 0
+        for p in planets:
+            self.nplanets += 1
+            for i, d in enumerate(p):
+                self.initpos[i].append(d)
+                self.initvel[i].append(0)
+    
+    def step_dim(self, pos, vel):
+        for i in range(self.nplanets):
+            for j in range(i+1, self.nplanets):
+                if pos[i] > pos[j]:
+                    vel[i] -= 1
+                    vel[j] += 1
+                elif pos[i] < pos[j]:
+                    vel[i] += 1
+                    vel[j] -= 1
+        for i in range(self.nplanets):
+            pos[i] += vel[i]
+    
+    def find_dim_repeat(self, d):
+        pos = deepcopy(self.initpos[d])
+        vel = deepcopy(self.initvel[d])
+        i = 1
+        self.step_dim(pos, vel)
+        while pos != self.initpos[d] or vel != self.initvel[d]:
+            i += 1
+            self.step_dim(pos, vel)
+        return i
+    
+    def find_repeat(self):
+        repeats = []
+        for d in range(3):
+            r = self.find_dim_repeat(d)
+            print("d=%d repeat=%d" % (d, r))
+            repeats.append(r)
+        return(lcmm(repeats[0], repeats[1], repeats[2]))
+    
+    
 if __name__ == '__main__':
     test1 = Day12([
                 Planet(-1, 0, 2),
@@ -75,37 +132,28 @@ if __name__ == '__main__':
                 Planet(4, -8, 8),
                 Planet(3, 5, -1)
             ])
-    test1.print_state()
-    test1b = deepcopy(test1)
-    for _ in range(10):
-        test1.step()
-    test1.print_state()
-    test1b.print_state()
-    #for p in test1.planets:
-    #    print(p)
-    print(test1.system_energy())
     
-    #test2 = Day12([
-    #            Planet(-8, -10, 0),
-    #            Planet(5, 5, 10),
-    #            Planet(2, -7, 3),
-    #            Planet(9, -8, -3)
-    #        ])
-    #for _ in range(100):
-    #    test2.step()
-    #for p in test2.planets:
-    #    print(p)
-    #print(test2.system_energy())
+    t1b = System([
+                [-1, 0, 2],
+                [2, -10, -7],
+                [4, -8, 8],
+                [3, 5, -1]
+            ])
+    print(t1b.find_repeat())
     
-    #inp = Day12([
-    #            Planet(-13, 14, -7),
-    #            Planet(-18, 9, 0),
-    #            Planet(0, -3, -3),
-    #            Planet(-15, 3, -13)
-    #        ])
-    #for _ in range(1000):
-    #    inp.step()
-    #for p in inp.planets:
-    #    print(p)
-    #print(inp.system_energy())
+    t2b = System([
+            [-8, -10,  0],
+            [ 5,   5, 10],
+            [ 2,  -7,  3],
+            [ 9,  -8, -3]
+        ])
+    print(t2b.find_repeat())
+    
+    inp = System([
+                (-13, 14,  -7),
+                (-18,  9,   0),
+                (  0, -3,  -3),
+                (-15,  3, -13)
+            ])
+    print(inp.find_repeat())
     
