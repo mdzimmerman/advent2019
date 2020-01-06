8f61bf1e7bbd91cd205bc0ad9440d72351f1482d
Matt Zimmerman <matt.zimmerman@ccri.com>
2020-01-06 10:38:42 -0500

    day 12 continued

--- 12/day12.py
+++ 12/day12.py
@@ -33,79 +33,111 @@ class Planet:
 class Day12:
     def __init__(self, planets):
         self.initplanets = planets
+        #self.planets = deepcopy(self.initplanets)
 
     def run(self, n):
-        self.t = 0
-        self.planets = deepcopy(self.initplanets)
+        t = 0
+        planets = deepcopy(self.initplanets)
+        #print(planets)
         for _ in range(n):
-            for i, a in enumerate(self.planets):
-                for b in self.planets[i+1:]:
+            for i, a in enumerate(planets):
+                #print(i, a)
+                for b in planets[i+1:]:
                     self.apply_gravity(a, b)
-            for p in self.planets:
+            for p in planets:
                 p.pos = p.pos.add(p.vel)
-            self.t += 1
+            t += 1
+        self.print_state(planets)
+        print('total energy = %d' % (self.system_energy(planets)))
+        return planets
 
     def apply_gravity(self, a, b):
         for dim in ['x', 'y', 'z']:
-            if getattr(a.pos, dim) > getattr(b.pos, dim):
-                setattr(a.vel, dim, getattr(a.vel, dim)-1)
-                setattr(b.vel, dim, getattr(b.vel, dim)+1)
-            elif getattr(a.pos, dim) < getattr(b.pos, dim):
-                setattr(a.vel, dim, getattr(a.vel, dim)+1)
-                setattr(b.vel, dim, getattr(b.vel, dim)-1)
-            else:
-                pass
+            self.apply_gravity_dim(a, b, dim)
+            
+    def apply_gravity_dim(self, a, b, dim):
+        if getattr(a.pos, dim) > getattr(b.pos, dim):
+            setattr(a.vel, dim, getattr(a.vel, dim)-1)
+            setattr(b.vel, dim, getattr(b.vel, dim)+1)
+        elif getattr(a.pos, dim) < getattr(b.pos, dim):
+            setattr(a.vel, dim, getattr(a.vel, dim)+1)
+            setattr(b.vel, dim, getattr(b.vel, dim)-1)
+        else:
+            pass
+
+    def step_dim(self, dim, planets):
+        for i, a in enumerate(planets):
+            for b in planets[i+1]:
+                self.apply_gravity_dim(a, b, dim)
+        for p in planets:
+            dpos = getattr(p.pos, dim)
+            dvel = getattr(p.vel, dim)
+            setattr(p.pos, dim, dpos + dvel)
+   
+    def run_dim(self, dim):
+        init_pos = tuple([getattr(p.pos, dim) for p in self.initplanets])
+        init_vel = tuple([getattr(p.vel, dim) for p in self.initplanets])
+        print(init_pos, init_vel)
+        pos = tuple([-1] * 4)
+        vel = tuple([-1] * 4)
+        i = 0
+        planets = deepcopy(self.initplanets)
+        print(planets)
+        while pos != init_pos and vel != init_vel:
+            i += 1
+            self.step_dim(dim, planets)
+            pos = tuple([getattr(p.pos, dim) for p in planets])
+            vel = tuple([getattr(p.vel, dim) for p in planets])
         
-    def system_energy(self):
+        print(i, init_pos, init_vel)
+        #print(init_pos)
+        #print(init_vel)
+        
+        #init_pos = getattr(self.initplanets.pos, dim)  # should be 0
+        #init_vel = getattr()
+        #while
+        
+    def system_energy(self, planets):
         te = 0
-        for p in self.planets:
+        for p in planets:
             te += p.te()
         return te
 
-    def print_state(self):
-        print("Day12(")
-        for p in self.planets:
-            print("  %s" % (p,))
-        print(")")
+    def print_state(self, planets):
+        for p in planets:
+            print("%s" % (p,))
 
 if __name__ == '__main__':
+    print("-- part 1 --")
+    print("test1")
     test1 = Day12([
                 Planet(-1, 0, 2),
                 Planet(2, -10, -7),
                 Planet(4, -8, 8),
                 Planet(3, 5, -1)
             ])
-    test1.print_state()
-    test1b = deepcopy(test1)
-    for _ in range(10):
-        test1.step()
-    test1.print_state()
-    test1b.print_state()
-    #for p in test1.planets:
-    #    print(p)
-    print(test1.system_energy())
+    test1.run(10)
     
-    #test2 = Day12([
-    #            Planet(-8, -10, 0),
-    #            Planet(5, 5, 10),
-    #            Planet(2, -7, 3),
-    #            Planet(9, -8, -3)
-    #        ])
-    #for _ in range(100):
-    #    test2.step()
-    #for p in test2.planets:
-    #    print(p)
-    #print(test2.system_energy())
+    print()
+    print("test2")
+    test2 = Day12([
+                Planet(-8, -10, 0),
+                Planet(5, 5, 10),
+                Planet(2, -7, 3),
+                Planet(9, -8, -3)
+            ])
+    test2.run(100)
     
-    #inp = Day12([
-    #            Planet(-13, 14, -7),
-    #            Planet(-18, 9, 0),
-    #            Planet(0, -3, -3),
-    #            Planet(-15, 3, -13)
-    #        ])
-    #for _ in range(1000):
-    #    inp.step()
-    #for p in inp.planets:
-    #    print(p)
-    #print(inp.system_energy())
+    print()
+    print("input")
+    inp = Day12([
+                Planet(-13, 14, -7),
+                Planet(-18, 9, 0),
+                Planet(0, -3, -3),
+                Planet(-15, 3, -13)
+            ])
+    inp.run(1000)
+    
+    print()
+    print("-- part 2 --")
     
